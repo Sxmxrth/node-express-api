3f8181bec5658353df923c2a0de699eab441a944
Sxmxrth <samarthgupta1202@gmail.com>
2022-11-19 09:50:19 +0530

    updated

--- index.js
+++ index.js
@@ -3,30 +3,14 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 const PORT = 5000;
-
-const users = [
-    {
-        firstName : "Samarth",
-        lastName : "Gupta",
-        age : "20"
-    },
-    {
-        firstName : "Navneet",
-        lastName : "Nair",
-        age : "19"
-    }
-
-]
 
 app.use("/users", usersRoutes);
 
 app.get("/", (req, res) => {
-    console.log(users);
     res.send("Hello from homepage")
 })
-
 
 app.listen(PORT, () => {
     console.log(`the server is up and running on port ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,32 @@ import express from "express"
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName : "Samarth",
+        lastName : "Gupta",
+        age : "20"
+    },
+    {
+        firstName : "Navneet",
+        lastName : "Nair",
+        age : "19"
+    }
+
+]
+
 //all routes in here are starting with /users
 router.get("/", (req, res) => {
     res.send("Hello")
+    // console.log(users);
 })
 
 router.post("/", (req, res) => {
-    res.send("Post is working")
+    console.log("POST ROUTE REACHED");
+    console.log(req.body);
+    users.push(req.body);
+    console.log(users);
+    res.send("POST ROUTE REACHED");
 })
 
 export default router;
